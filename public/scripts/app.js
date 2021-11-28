840e0791432404aaad061150be680cd4998edbfe
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-28 19:25:58 +0000

    Add function to hide/show extra menus

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,10 @@
 // Client facing scripts here
+
+// To show/hide extra menus when hamburger is clicked
+$('#hamburger').click(() => {
+  if ($('.navbar-pages').css('display') === 'none') {
+    $('.navbar-pages').css('display', 'block');
+  } else {
+    $('.navbar-pages').css('display', 'none');
+  }
+});
