12620274c672544584d0ea4a8eab78537099eee0
Andrew <andrew.spon@gmail.com>
2021-11-29 22:59:55 +0000

    finalize change

--- migration/1_schema.sql
+++ migration/1_schema.sql
@@ -1,12 +1,49 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(255),username VARCHAR(255), email VARCHAR(255), password VARCHAR(255));
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS answers CASCADE;
+DROP TABLE IF EXISTS quizzes CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS results CASCADE;
 
-CREATE TABLE catagories (id SERIAL PRIMARY KEY, name VARCHAR(255), description TEXT);
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE quizzes (id SERIAL PRIMARY KEY, catagory_id INTEGER, name VARCHAR(255), description TEXT, is_private BOOLEAN, creator_id VARCHAR(255));
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL
+);
 
-CREATE TABLE questions (id SERIAL PRIMARY KEY, quiz_id INTEGER,  question_text TEXT, correct_answer_id INTEGER);
+CREATE TABLE quizzes (
+  id SERIAL PRIMARY KEY,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  is_private BOOLEAN DEFAULT false,
+  creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
 
-CREATE TABLE answers (id SERIAL PRIMARY KEY, question_id INTEGER, answer_text TEXT);
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  question_text TEXT NOT NULL
+);
 
-CREATE TABLE results (id SERIAL PRIMARY KEY, quiz_id INTEGER, user_id INTEGER, correct_answers INTEGER);
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY,
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  answer_text TEXT NOT NULL,
+  correct_answer BOOLEAN DEFAULT false
+);
 
+CREATE TABLE results (
+  id SERIAL PRIMARY KEY,
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  total_correct  INTEGER DEFAULT 0
+);
