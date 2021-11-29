d09bac2935742911500c8e606bf746e64a0d7e5d
Andrew <andrew.spon@gmail.com>
2021-11-29 21:35:33 +0000

    fix schema && seeds

--- migration/1_schema.sql
+++ migration/1_schema.sql
@@ -2,9 +2,9 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(255),username VARCHAR(25
 
 CREATE TABLE catagories (id SERIAL PRIMARY KEY, name VARCHAR(255), description TEXT);
 
-CREATE TABLE quizzes (id SERIAL PRIMARY KEY, catagory_id INTEGER, name VARCHAR(255), description TEXT, status BOOLEAN, created_by VARCHAR(255));
+CREATE TABLE quizzes (id SERIAL PRIMARY KEY, catagory_id INTEGER, name VARCHAR(255), description TEXT, is_private BOOLEAN, creator_id VARCHAR(255));
 
-CREATE TABLE questions (id SERIAL PRIMARY KEY, correct_answer_id INTEGER, question_text TEXT);
+CREATE TABLE questions (id SERIAL PRIMARY KEY, quiz_id INTEGER,  question_text TEXT, correct_answer_id INTEGER);
 
 CREATE TABLE answer (id SERIAL PRIMARY KEY, question_id INTEGER, answer_text TEXT);
 
