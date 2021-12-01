de69b16e6b3eab6b11088a223b96c246442eb49c
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-12-01 22:59:01 +0000

    Remove categories table from database and reference to it

--- db/schema/3_quizzes.sql
+++ db/schema/3_quizzes.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS quizzes CASCADE;
 
 CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY,
-  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL,
   description TEXT,
   is_private BOOLEAN DEFAULT false,
--- server.js
+++ server.js
@@ -40,7 +40,6 @@ const questionsRoutes = require("./routes/questions");
 const answersRoutes = require("./routes/answers");
 const resultsRoutes = require("./routes/results");
 const quizzesRoutes = require("./routes/quizzes");
-const categoriesRoutes = require("./routes/categories")
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -49,7 +48,6 @@ app.use("/api/questions", questionsRoutes(db));
 app.use("/api/answers", answersRoutes(db));
 app.use("/api/results", resultsRoutes(db));
 app.use("/api/quizzes", quizzesRoutes(db));
-app.use("/api/categories", categoriesRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
