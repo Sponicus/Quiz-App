bf1cac8d0bfbb59287bc6ac895043694e66b9cd2
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-12-01 06:15:43 +0000

    Try to add a few querys for use on the taking quiz page

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -4,8 +4,10 @@ const router  = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`
-      SELECT quizzes.id AS quiz_id, quizzes.name AS quiz_name, quizzes.description AS description, users.name AS user_name FROM quizzes
-      JOIN users ON users.id = creator_id;
+    SELECT quizzes.id AS quiz_id, questions.id AS question_id, quizzes.name AS quiz_name, question_text AS question FROM quizzes
+    JOIN users ON users.id = creator_id
+    JOIN questions ON quizzes.id = quiz_id
+    WHERE questions.quiz_id = 1;
     `)
       .then(data => {
         const quizzes = data.rows;
@@ -20,3 +22,6 @@ module.exports = (db) => {
 
   return router;
 };
+
+/*SELECT quizzes.id AS quiz_id, quizzes.name AS quiz_name, quizzes.description AS description, users.name AS user_name FROM quizzes
+      JOIN users ON users.id = creator_id;*/
