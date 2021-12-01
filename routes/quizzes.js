711a45cd7c69661a9751a9dc17135469b3503801
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-12-01 01:20:34 +0000

    Revert back to simpler query since changed plan to redirect to new page

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -4,9 +4,8 @@ const router  = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`
-      SELECT quizzes.id AS quiz_id,quizzes.name AS quiz_name, quizzes.description AS description, users.name AS user_name, questions.id AS qid, questions.question_text as question FROM quizzes
-      JOIN users ON users.id = creator_id
-      JOIN questions ON quizzes.id = quiz_id;
+      SELECT quizzes.id AS quiz_id, quizzes.name AS quiz_name, quizzes.description AS description, users.name AS user_name FROM quizzes
+      JOIN users ON users.id = creator_id;
     `)
       .then(data => {
         const quizzes = data.rows;
@@ -19,26 +18,5 @@ module.exports = (db) => {
       });
   });
 
-  router.get("/take", (req, res) => {
-    db.query(`
-      SELECT answers.answer_text AS answer FROM questions
-      JOIN answers ON questions.id = question_id
-      WHERE question_id = ;
-    `)
-      .then(data => {
-        const quiz = data.rows;
-        res.json({ quiz });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
   return router;
 };
-
-/*
-SELECT quizzes.id AS quiz_id,quizzes.name AS quiz_name, quizzes.description AS description, users.name AS user_name FROM quizzes
-      JOIN users ON users.id = creator_id;
-*/
