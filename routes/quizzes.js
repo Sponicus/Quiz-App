f0bd6012bad625894d1420e19e45b27e90636091
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-12-01 06:18:05 +0000

    Test some queries before but now revert back to how it should be

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -4,10 +4,8 @@ const router  = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`
-    SELECT quizzes.id AS quiz_id, questions.id AS question_id, quizzes.name AS quiz_name, question_text AS question FROM quizzes
-    JOIN users ON users.id = creator_id
-    JOIN questions ON quizzes.id = quiz_id
-    WHERE questions.quiz_id = 1;
+      SELECT quizzes.id AS quiz_id, quizzes.name AS quiz_name, quizzes.description AS description, users.name AS user_name FROM quizzes
+      JOIN users ON users.id = creator_id;
     `)
       .then(data => {
         const quizzes = data.rows;
@@ -22,6 +20,3 @@ module.exports = (db) => {
 
   return router;
 };
-
-/*SELECT quizzes.id AS quiz_id, quizzes.name AS quiz_name, quizzes.description AS description, users.name AS user_name FROM quizzes
-      JOIN users ON users.id = creator_id;*/
