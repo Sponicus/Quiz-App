e80af6efc5fd1f533df8aba711fb349a9d23321d
Andrew <andrew.spon@gmail.com>
2021-12-02 17:48:42 +0000

    add short url to results

--- db/seeds/6_results.sql
+++ db/seeds/6_results.sql
@@ -1,2 +1,2 @@
-INSERT INTO results (quiz_id, user_id, total_correct)
-VALUES (1, 4, 2), (2, 3, 2), (1, 2, 1), (2, 2, 1);
+INSERT INTO results (quiz_id, user_id, short_url, total_correct)
+VALUES (1, 4,'short1', 2), (2, 3, 'short2', 2), (1, 2, 'short3', 1), (2, 2, 'short4', 1);
