8e6dcf49a812b850cecfd0a8b77b1db0a2dc7551
Andrew <andrew.spon@gmail.com>
2021-12-02 00:47:20 +0000

    implement queries for database insertion from Create Quiz

--- routes/create.js
+++ routes/create.js
@@ -11,22 +11,36 @@ module.exports = (db) => {
     res.render("create_quiz", templateVars);
   })
 
-  router.post("/", (req,res) => {
-    const questions = req.body.question;
-    const answers = req.body.answers;
-    const userVals = '3' //placeholder
-    const currentUser = db.query(`SELECT users.id FROM users WHERE users.id = 1$`, [userVals])
-    const currentQuiz = db.query(`INSERT INTO quizzes VALUES () `)
-
-
-
-    // for (const question of questions) {INSERT INTO questions VALUES ()}    //loop through questions
-    //   //insert questions in DB
-    //   //Query its ID
-    //   //.then
-    //   //For EachLoop through answers
-    //     //insert answers into DB
-    res.redirect("/create");
+  router.post("/", async (req,res) => {
+    //set constants for data to be queried
+    const {question, answer, description, name="a cool name for quiz"} = req.body;
+    console.log({question, answer, description, name})
+    const userID= '3'; //placeholder
+    try {
+      const resQuiz = await db.query(`INSERT INTO quizzes (name, description, is_private, creator_id) VALUES ($1, $2, true, $3) RETURNING id`, [name, description, userID])
+        console.log({res: resQuiz})
+        const quizID = resQuiz.rows[0].id;
+        try {
+          for (const q in question) {
+            const resQuestions = await db.query(`INSERT INTO questions (quiz_id, question_text) VALUES ($1,$2) RETURNING id`, [quizID, question[q]])
+              const questionID = resQuestions.rows[0].id
+              try {
+                for (const a in answer[q]) {
+                  await db.query(`INSERT INTO answers (question_id, answer_text) VALUES ($1, $2)`, [questionID, answer[q][a]])
+                }
+              } catch (err) {
+                console.log(err.stack)
+              }
+          }
+        } catch (err) {
+          console.log(err.stack)
+        }
+      ;
+      res.redirect("/create");
+    } catch (err) {
+      console.log(err.stack)
+      res.send('something went wrong!!! :(')
+    }
   })
 
   return router;
