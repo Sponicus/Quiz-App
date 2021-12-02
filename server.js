a7444583ffaba76ac3a05d8642f90afc9865ca83
Andrew <andrew.spon@gmail.com>
2021-12-02 20:09:51 +0000

    add logout feature

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ const answersRoutes = require("./routes/answers");
 const resultsRoutes = require("./routes/results");
 const quizzesRoutes = require("./routes/quizzes");
 const createRoutes = require("./routes/create");
-const loginRoutes = require("./routes/login")
+const loginRoutes = require("./routes/login");
+const logoutRoutes = require("./routes/logout");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -61,6 +62,7 @@ app.use("/api/results", resultsRoutes(db));
 app.use("/api/quizzes", quizzesRoutes(db));
 app.use("/create", createRoutes(db));
 app.use("/login", loginRoutes(db));
+app.use("/logout", logoutRoutes());
 
 // Note: mount other resources here, using the same pattern above
 
