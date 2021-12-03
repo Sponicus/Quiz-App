c4cf262430bb759a7686b0b090487631b51de856
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-12-03 06:35:08 +0000

    Add cookieUser as key in templateVars

--- routes/user_quizzes.js
+++ routes/user_quizzes.js
@@ -4,8 +4,11 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    
-    res.render("user_quizzes")
+    const templateVars = {
+      cookieUser: req.session.user_id
+    };
+
+    res.render("user_quizzes", templateVars);
   });
 
   return router;
